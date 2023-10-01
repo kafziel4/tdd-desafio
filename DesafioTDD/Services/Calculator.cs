6b1fb5c5e3cabfab6a499950dadcd7b7d2066382
Keoma Kevin Baudin <keoma4@gmail.com>
2023-10-01 20:28:12 -0300

    feat: implement Sum

--- DesafioTDD/Services/Calculator.cs
+++ DesafioTDD/Services/Calculator.cs
@@ -4,7 +4,8 @@
     {
         public int Sum(int value1, int value2)
         {
-            return -1;
+            int result = value1 + value2;
+            return result;
         }
 
         public int Subtract(int value1, int value2)
